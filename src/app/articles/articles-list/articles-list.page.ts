b9e7ff5a4ee10747181ceb561e732478f3db71d8
sungwoonko <ko_sw@naver.com>
2025-02-10 12:50:53 +0900

    feat : 게시글 조회 Fetch API 요청문 작성과 데이터 바인딩 구현

--- src/app/articles/articles-list/articles-list.page.ts
+++ src/app/articles/articles-list/articles-list.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from 'src/app/common/article-response.interface';
+import { ArticlesService } from '../articles.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-articles-list',
@@ -7,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   standalone: false
 })
 export class ArticlesListPage implements OnInit {
+  articles: Article[] = [];
 
-  constructor() { }
+  constructor(
+    private articlesService: ArticlesService,
+    private router: Router,
+  ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    try {
+      const response = await this.articlesService.getAllArticles();
+      if(response.sucess) {
+        this.articles = response.data;
+      }else{
+        console.error(response.message);
+      }
+
+    } catch (error) {
+      console.error('Fetch error',error);      
+    }
   }
 
+  //route Article detail
+  viewArticle(id: number){
+    this.router.navigate([`${id}/detail`]);
+  }
 }
--- src/app/articles/articles.service.ts
+++ src/app/articles/articles.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { ApiResponse } from '../common/api-response.interface';
+import { Article } from '../common/article-response.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticlesService {
+  private apiUrl = 'http://localhost:3000/api/articles'
 
-  constructor() { }
+  async getAllArticles(): Promise<ApiResponse<Article[]>> {
+    try {
+      const response = await fetch(`${this.apiUrl}`);
+
+      if(!response.ok){
+        throw new Error('Network response was not ok');
+      }
+      const data: ApiResponse<Article[]> = await response.json();
+      return data;    
+    } catch (error) {
+      console.error('Fetch error',error)
+      throw error;
+    }
+
+  }
 }
